a7ef67d07963541358acab808dd8661386ec8989
Sohrab Tawana <sohrab.tawana@steadforce.com>
2023-07-12 09:48:08 +0200

    Implement lombok and remove setter getters.

--- backend/src/main/java/org/acme/Team.java
+++ backend/src/main/java/org/acme/Team.java
@@ -2,12 +2,14 @@ package org.acme;
 
 import jakarta.persistence.*;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity(name = "Team")
 @Table(name = "teams")
+@Getter
+@Setter
 public class Team {
-
-    // attributes
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "team_id")
@@ -39,53 +41,4 @@ public class Team {
         this.pointsScored = 0;
         this.pointsAllowed = 0;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setWins(int wins) {
-        this.wins = wins;
-    }
-
-    public int getWins() {
-        return this.wins;
-    }
-
-    public void setLosses(int losses) {
-        this.losses = losses;
-    }
-
-    public int getLosses() {
-        return this.losses;
-    }
-
-    public void setPointsScored(int pointsScored) {
-        this.pointsScored = pointsScored;
-    }
-
-    public int getPointsScored() {
-        return this.pointsScored;
-    }
-
-    public void setPointsAllowed(int pointsAllowed) {
-        this.pointsAllowed = pointsAllowed;
-    }
-
-    public int getPointsAllowed() {
-        return this.pointsAllowed;
-    }
-
 }
