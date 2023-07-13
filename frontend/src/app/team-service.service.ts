43842e8a09ab8d3933ebd2acf845e726c9a1c19f
Sohrab Tawana <sohrab.tawana@steadforce.com>
2023-07-13 13:16:08 +0200

    Implement docker environment for angular.

--- frontend/src/app/team-service.service.ts
+++ frontend/src/app/team-service.service.ts
@@ -2,32 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Team } from './team-table/team-table.component';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
-  private apiUrl = 'http://host.docker.internal:8080/api';
 
   constructor(private http: HttpClient) {}
 
   getTeams(): Observable<Team[]> {
-    const url = `${this.apiUrl}/get_teams`;
+    const url = `${environment.apiUrl}/get_teams`;
     return this.http.get<Team[]>(url);
   }
 
   addTeam(team: Partial<Team>): Observable<Team> {
-    const url = `${this.apiUrl}/add_team`;
+    const url = `${environment.apiUrl}/add_team`;
     return this.http.post<Team>(url, team);
   }
 
   deleteTeam(teamId: number): Observable<Team> {
-    const url = `${this.apiUrl}/delete_team`;
+    const url = `${environment.apiUrl}/delete_team`;
     return this.http.post<Team>(url, teamId);
   }
 
   updateTeam(team: Partial<Team>): Observable<Team> {
-    const url = `${this.apiUrl}/update_team`;
+    const url = `${environment.apiUrl}/update_team`;
     return this.http.post<Team>(url, team);
   }
 }
